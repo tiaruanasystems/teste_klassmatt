1506b3947a809173028660eacbf232dee8d47c18
ti_aruanasystems <ti.aruanasystems@gmail.com>
2022-08-31 21:14:14 -0300

    feat

---
+++ Teste_klassmatt/Teste_klassmatt/Dominio/JogoDaVelha.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste_klassmatt.Dominio
+{
+    public class JogoDaVelha
+    {
+           
+        public bool fimDeJogo  { get; set; }
+        public char[] posicoes { get; set; }
+        public char vez { get; set; }
+        public int quantidadePreenchida { get; set; }
+
+        public JogoDaVelha()
+        {
+            fimDeJogo = false;
+            posicoes = new[] { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            vez = 'X';
+            quantidadePreenchida = 0;
+        }
+
+
+
+    }
+}
---
+++ Teste_klassmatt/Teste_klassmatt/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Teste_klassmatt.Dominio;
+
+namespace Teste_klassmatt
+{
+    internal class Program
+    {
+
+        private bool fimDeJogo = false;
+
+        static void Main()
+        {
+            new Jogo().Iniciar();
+        }
+
+    }
+}
